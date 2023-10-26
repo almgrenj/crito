03ab5f47d0d0fdab298368c653212d51546d0539
Jacob <almgrenj@proton.me>
2023-10-26 15:50:16 +0200

    changed som design

--- src/components/BoxContainerSection/BoxContainerSection.js
+++ src/components/BoxContainerSection/BoxContainerSection.js
@@ -1,34 +1,33 @@
-import React from 'react'
-import '../Contact.css';
-
+import React from 'react';
+import '../Contact.css'; 
 const BoxContainerSection = () => {
   return (
     <div className="box-container">
-    <div className="box">
+      <div className="box">
         <i className="fas fa-map-marker-alt box-icon"></i> 
         <div className="box-text-content">
-            <h2>Visit Us</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+          <h2>Visit Us</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
         </div>
-    </div>
-    
-    <div className="box">
+      </div>
+      
+      <div className="box">
         <i className="fas fa-phone-alt box-icon"></i>
         <div className="box-text-content">
-            <h2>Call Us</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+          <h2>Call Us</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
         </div>
-    </div>
-    
-    <div className="box">
+      </div>
+      
+      <div className="box">
         <i className="fas fa-envelope box-icon"></i>
         <div className="box-text-content">
-            <h2>Email Us</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+          <h2>Email Us</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
         </div>
+      </div>
     </div>
-</div>
-  )
+  );
 }
 
-export default BoxContainerSection
+export default BoxContainerSection;
