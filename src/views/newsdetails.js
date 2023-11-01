0097e219242be53795e337fd643ed946b0e518b9
Jacob <almgrenj@proton.me>
2023-11-01 16:45:09 +0100

    added things

--- src/views/newsdetails.js
+++ src/views/newsdetails.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
+import TopBar from '../components/TopBar/TopBar';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+import HeroNewsSection from '../components/HeroNewsSection/HeroNewsSection';
+import ArticleSection from '../components/Article/ArticleSection';
+
 const newsdetails = () => {
   return (
-    <div>newsdetails</div>
+    <div>
+      <TopBar />
+      <Header />
+      <HeroNewsSection />
+      <ArticleSection />
+      <Footer />
+    </div>
   )
 }
 
